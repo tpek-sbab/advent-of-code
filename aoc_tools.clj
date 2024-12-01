e14f27f450c73fa76232a2bd762952e8242f265d
Tomas Petkov <tomas.petkov@sbab.se>
2024-12-01 15:02:21 +0100

    Setup

---
+++ aoc_tools.clj
@@ -0,0 +1,83 @@
+(ns aoc-tools
+  (:require
+   [utils :refer [extract-numbers parse-int]]
+   [clj-http.client :as client]
+   [clojure.string :as str]
+   [clojure.java.io :as io]))
+
+(defn extract-example-text
+  [body]
+  (->> body
+       (re-seq #"(?is)<pre><code>(.*?)\n? *?</code></pre>")
+       (first)
+       (last)))
+
+(defn fetch-input
+  [year day]
+  (let [base-url       (str "https://adventofcode.com/20" year "/day/" day)
+        input-url      (str base-url "/input")
+        cookie         (slurp "token.txt")
+        payload        {:headers {:Cookie     cookie
+                                  :User-Agent "Tomas' AoC Clojure Library https://github.com/tpek-sbab/advent-of-code"}}
+        base-response  (client/get base-url payload)
+        example-text   (extract-example-text (:body base-response))
+        input-response (client/get input-url payload)
+        input-text     (:body input-response)]
+    [example-text input-text]))
+
+
+(defn create-files
+  ([day]
+   (let [year (->> (io/file ".")
+                   (.list)
+                   (filter #(re-seq #"aoc\d\d" %))
+                   (sort)
+                   (last)
+                   (re-seq #"\d+")
+                   (first))]
+     (create-files (parse-int year) day)))
+
+  ([year day]
+   (let [year       (mod year 2000)
+         input-path (str "aoc" year "/inputs/")
+         src-path   (str "aoc" year "/src/")
+         [example-text input-text] (fetch-input year day)]
+     (do
+       (spit (str input-path "day" day ".ex") example-text)
+       (spit (str input-path "day" day ".txt") input-text)
+       (spit (str src-path "day" day ".clj") (str "(ns aoc" year ".src.day" day "\n"
+                                                  "  (:require \n"
+                                                  "    [aoc-tools :refer [read-input submit-answer]]))\n\n"
+                                                  "(def input (read-input :test))\n"
+                                                  "(def real-input (read-input))\n\n"))))))
+
+(defn get-year-and-day
+  []
+  (let [[project _ file] (str/split (str *ns*) #"\.")
+        year (first (extract-numbers project))
+        day  (first (extract-numbers file))]
+    [year day]))
+
+(defn read-input
+  [& [arg]]
+  (let [[year day] (get-year-and-day)
+        file (cond
+               (nil? arg) (str "day" day ".txt")
+               (= arg :test) (str "day" day ".ex")
+               :else (str (name arg)))
+        path (str "aoc" year "/inputs/" file)]
+    (->> (with-open [rdr (io/reader path)]
+           (mapv str (line-seq rdr))))))
+
+(defn submit-answer
+  [part answer]
+  (let [[year day] (get-year-and-day)
+        url      (str "https://adventofcode.com/20" year "/day/" day "/answer")
+        cookie   (slurp "token.txt")
+        payload  {:method      "POST"
+                  :headers     {:Cookie     cookie
+                                :User-Agent "Tomas' AoC Clojure Library https://github.com/tpek-sbab/advent-of-code"}
+                  :form-params {"level" (str part) "answer" (str answer)}}
+        response (client/post url payload)
+        _        (println "Submitting answer" answer)]
+    (re-seq #"(?is).{20}answer.{50}" (:body response))))
---
+++ utils.clj
@@ -0,0 +1,126 @@
+(ns utils)
+
+(defn parse-int [s]
+  (cond
+    (char? s) (Integer/parseInt (str s))
+    (= s "") 0
+    :else (Integer/parseInt s)))
+
+(defn extract-numbers
+  [string]
+  (->> string
+       (re-seq #"-?\d+")
+       (map parse-int)))
+
+(defn intstring?
+  [s]
+  (re-seq #"^-?\d+$" (str s)))
+
+(defn in?
+  [coll elm]
+  (some #(= elm %) coll))
+
+(defn get-adjacent
+  [n [x y]]
+  (case n
+    4 (list [(inc x) y] [(dec x) y] [x (inc y)] [x (dec y)])
+    5 (conj (get-adjacent [x y] 4) [x y])
+    8 (concat (get-adjacent [x y] 4) [[(inc x) (inc y)] [(inc x) (dec y)] [(dec x) (inc y)] [(dec x) (dec y)]])
+    9 (conj (get-adjacent [x y] 8) [x y])))
+
+(defn non-neg?
+  [x]
+  (>= x 0))
+
+(defn non-pos?
+  [x]
+  (<= x 0))
+
+(defn nmap
+  ([f coll]
+   (nmap 2 f coll))
+  ([n f coll]
+   (if (= n 1)
+     (map f coll)
+     (map #(nmap (dec n) f %) coll))))
+
+(defn rotate
+  [[head & tail]]
+  (concat tail (list head)))
+
+(defn ->!
+  [f & args]
+  (apply f (last args) (butlast args)))
+
+(defn ->>!
+  [arg f & args]
+  (apply f (concat args [arg])))
+
+(defn filter-keys
+  [f m]
+  (->> m
+       (filter (fn [[key value]] (f key)))
+       (into {})))
+
+(defn filter-vals
+  [f m]
+  (->> m
+       (filter (fn [[key value]] (f value)))
+       (into {})))
+
+(defn mfilter
+  ([f m]
+   (mfilter :keys f m))
+  ([by f m]
+   (case by
+     :keys (filter-keys f m)
+     :vals (filter-vals f m))))
+
+(defn mremove
+  ([f m]
+   (mremove :keys f m))
+  ([by f m]
+   (mfilter by (complement f) m)))
+
+(defn iterate-until
+  [condition f start-val]
+  (->> start-val
+       (iterate f)
+       (take-while (complement condition))
+       (last)
+       (f)))
+
+(defn indices
+  ([coll]
+   (indices coll 0 0))
+  ([coll drop-start drop-end]
+   (->> (range (count coll))
+        (drop drop-start)
+        (drop-last drop-end))))
+
+(defn second-last
+  [coll]
+  (last (butlast coll)))
+
+(defn transpose
+  [list-of-seqs]
+  (let [transposed (->> list-of-seqs
+                        (apply mapv vector))]
+    (if (some string? list-of-seqs)
+      (map #(apply str %) transposed)
+      transposed)))
+
+(defn remove-first
+  [item-or-pred coll]
+  (let [pred (if (ifn? item-or-pred)
+               item-or-pred
+               #{item-or-pred})
+        [before after] (split-with (complement pred) coll)]
+    (concat before (rest after))))
+
+(defn sgn
+  [x]
+  (cond
+    (pos? x) 1
+    (neg? x) -1
+    :else 0))
