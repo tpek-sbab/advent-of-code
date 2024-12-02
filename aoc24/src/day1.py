dd79acf2ad8684f89b83ab28e9a28375b38fc3e7
Tomas Petkov <tomas.petkov@sbab.se>
2024-12-02 11:45:54 +0100

    Add python solutions

---
+++ aoc24/src/day1.py
@@ -0,0 +1,22 @@
+with open("aoc24/inputs/day1.txt") as f:
+    lines = f.read().splitlines()
+
+left = []
+right = []
+
+for line in lines:
+    a, b = line.split("   ")
+    left.append(int(a))
+    right.append(int(b))
+
+left.sort()
+right.sort()
+
+part_1 = part_2 = 0
+
+for i in range(len(left)):
+    part_1 += abs(left[i] - right[i])
+    part_2 += left[i] * right.count(left[i])
+
+print(f"Part 1: {part_1}")
+print(f"Part 2: {part_2}")
---
+++ aoc24/src/day2.py
@@ -0,0 +1,29 @@
+with open("aoc24/inputs/day2.txt") as f:
+    lines = f.read().splitlines()
+
+reports = [[int(i) for i in line.split(" ")] for line in lines]
+
+
+def is_safe(report: list[int]) -> bool:
+    diffs = {report[i] - report[i + 1] for i in range(len(report) - 1)}
+    return diffs.issubset({1, 2, 3}) or diffs.issubset({-1, -2, -3})
+
+
+def dampened_is_safe(report: list[int]) -> bool:
+    if is_safe(report):
+        return True
+
+    for i in range(len(report)):
+        copy = report[:]
+        copy.pop(i)
+        if is_safe(copy):
+            return True
+
+    return False
+
+
+part_1 = len([report for report in reports if is_safe(report)])
+part_2 = len([report for report in reports if dampened_is_safe(report)])
+
+print(f"Part 1: {part_1}")
+print(f"Part 2: {part_2}")
