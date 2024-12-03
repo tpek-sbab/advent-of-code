ea96fdd10d2158aa1417188c8c6ed16e722ddf5a
Tomas Petkov <tomas.petkov@sbab.se>
2024-12-03 09:52:47 +0100

    Day 3 Clojure

---
+++ aoc24/src/day3.clj
@@ -0,0 +1,32 @@
+(ns aoc24.src.day3
+  (:require
+   [aoc-tools :refer [read-input submit-answer]]
+   [utils :refer [nmap extract-numbers]]
+   [clojure.string :as str]))
+
+(def input (read-input :test))
+(def real-input (read-input))
+
+(defn filter-instructions
+  [s]
+  (->> s
+       (str "do()")
+       (#(str/replace % #"do\(\)" "\ndo()"))
+       (#(str/replace % #"don't\(\)" "\ndon't()"))
+       (#(str/split % #"\n"))
+       (filter #(str/starts-with? % "do()"))
+       (apply str)))
+
+;; Part 1 and 2
+(->> real-input
+     (apply str)
+     (filter-instructions) ;; Comment out for part 1
+     (re-seq #"mul\(\d+,\d+\)")
+     (map #(re-seq #"\d+" %))
+     (map #(*
+            (Integer/parseInt (first %))
+            (Integer/parseInt (second %))))
+     (apply +)
+     #_(submit-answer 2))
+
+
